2faae3a838ed31e4162518301fcefad180fadba9
Shrish-M <mahesh49@purdue.edu>
2025-04-12 13:15:23 -0400

    Outline

---
+++ extractAndCompare.py
@@ -0,0 +1,20 @@
+
+
+#image - extract image hardcoded for now
+
+
+
+#clean - reduce noise, object detection (annotations), maybe OCR if applicable
+
+
+
+#compare rubric
+
+#call function to get rubric and points deductions
+
+
+
+#general-response from a multimodal LLM that can anaylze our image
+
+
+
